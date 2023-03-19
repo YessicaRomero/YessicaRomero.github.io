8dff869739a27535ff7d592383b0ce41ea288ff3
Yessica Romero <124018374+YessicaRomero@users.noreply.github.com>
2023-03-19 17:34:55 -0300

    initial commit

---
+++ tercerho.js
@@ -0,0 +1,4 @@
+document.getElementById('boton').onclick = () => {
+    console.log("capturamos el evento clik");
+document.getElementById("demo").innerHTML = "Yessica Romero 30años email:yessilpd91@gmail.com";
+}
